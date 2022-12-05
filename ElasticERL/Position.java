4dc994da008f7057eb56012024c5178520b7aae1
huy227 <huy2272@gmail.com>
2022-12-04 21:22:21 -0500

    Implemented add(), the method is currently static not sure how it would work once we create an instance of ElasticERL

---
+++ ElasticERL/Position.java
@@ -0,0 +1,6 @@
+package ElasticERL;
+//position returns element
+abstract class Position<E> {
+	abstract E getElement();
+	abstract E getValue();
+}
--- Position.java
+++
@@ -1,5 +0,0 @@
-package ElasticERL;
-//position returns element
-abstract class Position<E> {
-	abstract E getElement();
-}
